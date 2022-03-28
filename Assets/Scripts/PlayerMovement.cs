c0d13450949f07e936b2b357df7243ada3f4bd4a
Henrique Loureiro de Faria <henriquelfaria08@gmail.com>
2022-03-28 17:21:02 -0300

    Player horizontal movement and flipSprite

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,40 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float runSpeed = 10;
     Vector2 moveInput;
+    Rigidbody2D rgb;
 
     void Start()
     {
-        
+        rgb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, rgb.velocity.y);
+        rgb.velocity = playerVelocity;
+    }
+
+    void FlipSprite()
+    {
+        bool playerMoving = Mathf.Abs(rgb.velocity.x) > Mathf.Epsilon;
+
+        if(playerMoving)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(rgb.velocity.x), 1f);
+        }
     }
 }
