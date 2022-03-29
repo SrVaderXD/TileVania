27d6572c2d8a8fc451f63849d68aa8b6a149aff6
Henrique Loureiro de Faria <henriquelfaria08@gmail.com>
2022-03-29 14:25:50 -0300

    Added Jump Movement for the Player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float runSpeed = 10;
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
     Vector2 moveInput;
     Rigidbody2D rgb;
+    Animator animator;
+    CapsuleCollider2D capsuleCollider;
 
     void Start()
     {
         rgb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
@@ -26,10 +31,23 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log(moveInput);
     }
 
+    void OnJump(InputValue value)
+    {
+        if(!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {return;}
+
+        if(value.isPressed)
+        {
+            rgb.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
     void Run()
     {
         Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, rgb.velocity.y);
         rgb.velocity = playerVelocity;
+
+        bool playerMoving = Mathf.Abs(rgb.velocity.x) > Mathf.Epsilon;
+        animator.SetBool("isRunning", playerMoving);
     }
 
     void FlipSprite()
